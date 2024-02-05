f0cf746312c23bcbd5b1fd9d700dfae7868a219f
chintan shah <chintan@8848digital.com>
2024-02-05 11:56:58 +0530

    feat: Initalized app and added fixtures for Supplier

--- mdm_sync_app/hooks.py
+++ mdm_sync_app/hooks.py
@@ -227,3 +227,10 @@ app_license = "mit"
 # 	"Logging DocType Name": 30  # days to retain logs
 # }
 
+fixtures = [
+    {"dt": "Custom Field", "filters": [
+        [
+            "module", "=", "MDM Sync App"
+        ]
+    ]}
+]
